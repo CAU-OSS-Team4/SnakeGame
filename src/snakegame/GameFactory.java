46947f0e993b7175c26fe95cebd6a72cf5cfc462
HyunJinNo <nhj9831@naver.com>
2022-05-26 14:23:11 +0900

    AUTO PLAY와 관련된 게임 시작 정보 수정.

--- src/snakegame/GameFactory.java
+++ src/snakegame/GameFactory.java
@@ -39,21 +39,16 @@ public class GameFactory {
 	}
 	public static Game createAutoMode() {
 		int width = 40;
-		int height = 80;
+		int height = 40;
 		
-		Player[] players = new Player[2];
+		Player[] players = new Player[1];
 		
 		Deque snake = new Deque(width * height + 1);
-		snake.push_front(new Pair(0, 0));
-		players[0] = new Player(snake, DIRECTION.SOUTH, 0, false, PlayerType.PLAYER1);
+		snake.push_front(new Pair(width / 2, height / 2));
+		players[0] = new Player(snake, DIRECTION.NORTH, 0, false, PlayerType.AUTO);
 
-		snake = new Deque(width * height + 1);
-		snake.push_front(new Pair(width - 1, height - 1));
-		players[1] = new Player(snake, DIRECTION.NORTH, 0, false, PlayerType.AUTO);
-		
-		Pair[] apples = new Pair[2];
+		Pair[] apples = new Pair[1];
 		apples[0] = new Pair(-1, -1);
-		apples[1] = new Pair(-1, -1);
 		
 		GameContext ctx = new GameContext(width, height, players, apples);
 		return new Game(ctx, true);

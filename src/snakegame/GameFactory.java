ddd3428a4784c75fbb7b2f4fb6d9b029255b68ab
angello0510 <angello0510@gmail.com>
2022-05-29 02:03:24 +0900

    Fix auto play mode bug

--- src/snakegame/GameFactory.java
+++ src/snakegame/GameFactory.java
@@ -40,13 +40,18 @@ public class GameFactory {
 		int width = 40;
 		int height = 40;
 		
-		Player[] players = new Player[1];
+		Player[] players = new Player[2];
 		Deque snake = new Deque(width * height + 1);
-		snake.push_front(new Pair(width / 2, height / 2));
-		players[0] = new Player(snake, DIRECTION.NORTH, 0, false, PlayerType.AUTO);
+		snake.push_front(new Pair(0, 0));
+		players[0] = new Player(snake, DIRECTION.SOUTH, 0, false, PlayerType.PLAYER1);
 
-		Pair[] apples = new Pair[1];
+		snake = new Deque(width * height + 1);
+		snake.push_front(new Pair(width - 1, height - 1));
+		players[1] = new Player(snake, DIRECTION.NORTH, 0, false, PlayerType.AUTO);
+		
+		Pair[] apples = new Pair[2];
 		apples[0] = new Pair(-1, -1);
+		apples[1] = new Pair(-1, -1);
 		
 		GameContext ctx = new GameContext(width, height, players, apples);
 		return new Game(ctx, true);
